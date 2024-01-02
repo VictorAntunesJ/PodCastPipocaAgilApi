3abe8475d1d8786f5bc26a113646117fe0c0de00
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-01-01 22:38:50 -0300

    Implementadando Crud de acompanhamento de lives.

---
+++ TestPipicaAgil/TestControlles.cs
@@ -0,0 +1,22 @@
+// using System;
+
+// using Xunit;
+
+// namespace TestPipicaAgil
+// {
+//     public class TestControllers
+//     {
+//         private Controllers controllers;
+
+//         public UserControllersTest()
+//         {
+//             userCadastroController = new Controllers(
+//                 new Mock<>(Cadastro).Object,
+//                 new Mock<IMapper>().Object
+//             );
+//         }
+
+//         [Fact]
+//         public void Test1() { }
+//     }
+// }
---
+++ TestPipicaAgil/TestsController.cs
@@ -0,0 +1,18 @@
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+
+// namespace TestPipicaAgil
+// {
+//     public class TestsController
+//     {
+//         private CadastroController cadastroController;
+
+//         public TestsController()
+//         {
+//             cadastroController = new CadastroController(new Mock<>)
+//         }
+
+//     }
+// }
