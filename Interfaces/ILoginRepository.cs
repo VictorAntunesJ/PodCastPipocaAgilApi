8ac40e47d2b5e3d2d887c9904c2f2751f3cb18ae
Victor Antunes J <victorsergioantunes23@gmail.com>
2024-01-04 01:23:59 -0300

    Refatoracao classe CSPROJ

--- Interfaces/ILoginRepository.cs
+++
@@ -1,9 +0,0 @@
-using PodCastPipocaAgilApi.Models;
-
-namespace PodCastPipocaAgilApi.Interfaces
-{
-    public interface ILoginRepository
-    {
-        string Logar(string email, string senha);
-    }
-}
--- SendEmail/IMailService.cs
+++
@@ -1,7 +0,0 @@
-namespace PodCastPipocaAgilApi.SendEmail
-{
-    public interface IMailService
-    {
-        void SendEmail(string[] emails, string subject, string body, bool isHtml = false);
-    }
-}
